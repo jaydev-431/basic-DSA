88cf3194ea4b4620d64cbbf07d6f7f7cd8867e99
jaydev <sreevatsalsreevatsal@gmail.com>
2026-02-07 07:56:29 +0530

    Delete Desktop/java directory

--- Desktop/java/DSA/Get_ith_bit.java
+++
@@ -1,8 +0,0 @@
-public class Get_ith_bit {
-    public static void main(String args[]){
-        int n=15;
-        int ith_position = 2;
-        int bit = (n&(1<<ith_position));
-        System.out.println(bit!=0 ? "1":"0");
-    }
-}
